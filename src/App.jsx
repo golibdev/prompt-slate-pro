64fcad7e5528e0f022b1b9c00b08ba35f2540b8a
golibdev <golibyaxshilikov1@gmail.com>
2026-07-02 05:33:36 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        background: "#15171A",
+        minHeight: "100vh",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "32px",
+        fontWeight: "bold",
+      }}
+    >
+      Prompt Slate Pro 🚀
+    </div>
+  );
+}
